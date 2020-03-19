7571b67d62a343a51dcb2bbeb4634b342e00d4a0
Rebecca <rebeayto@gmail.com>
2020-03-19 19:45:07 +0000

    hides folder input after selection & alphabetical sorting

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 document.querySelector("input").onchange = function() {
     console.log(this.files);
+    hideInput();
 
     let re = /video\/*/;
-    [].slice.call(this.files).forEach( function(v) {
+
+    var filelist = [].slice.call(this.files);
+    filelist.sort(function(a, b) {
+        return (a.name < b.name ? -1 : (a.name > b.name ? 1 : 0));
+    });
+    
+    filelist.forEach( function(v) {
         var link = "file:///home/rla/" + v.webkitRelativePath;
         if(re.test(v.type)) {
             $("body").append(`<div><a href="${link}"> ${v.name} </a></div>`);
         }
     });
 };
+
+function hideInput() {
+    $("#folderInput").toggle();
+}
