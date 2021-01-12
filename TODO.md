00b2a8b65ce53850ced22e1c3eabc17a22678469
Rebecca <rebeayto@gmail.com>
2021-01-12 15:26:50 +0000

    Basic node object added

---
+++ TODO.md
@@ -0,0 +1,7 @@
+#TODO
+
+-group by show name
+-regex to clear up names
+-get show icons
+
+-watched on open / unwatch button
--- script.js
+++ script.js
@@ -1,24 +1,36 @@
+class Node {
+  contructor(name, link) {
+    this.name = name;
+    this.link = link;
+    children = []
+  }
+
+  get(name) {
+    children.forEach((item, i) => {
+      if (item.name == name) {
+        return children[i]
+      }
+    });
+
+    children.push(new Node(name, ""))
+  }
+}
+
 const {shell} = require('electron')
-
-
 
 const submitListener = document
   .querySelector('form')
   .addEventListener('submit', (event) => {
+      //TODO: remove previous files in div
+
+
       event.preventDefault()
 
       const files = [...document.getElementById('filePicker').files]
       let re = /video\/*/;
-
-      let div = document.createElement("div");
-      var a = document.createElement("a");
-      var link = document.createTextNode("test");
-
-      a.appendChild(link);
-
-      a.href = "https://www.google.com";
-      div.appendChild(a);
-      document.body.appendChild(div)
+      // files.sort(function(a, b) {
+      //   return (a.webkitRelativePath < b.webkitRelativePath ? -1 : (a.webkitRelativePath > b.webkitRelativePath ? 1 : 0));
+      // });
 
       files.forEach((item, i) => {
         if (re.test(item.type)) {
@@ -48,8 +60,5 @@ const submitListener = document
           })
         }
       })
-
-
-
 
   })
